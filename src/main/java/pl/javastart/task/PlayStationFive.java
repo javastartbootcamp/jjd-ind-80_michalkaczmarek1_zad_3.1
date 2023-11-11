cfbea0a762b6ba433a00a9878f401d1db05eddc6
Michał Kaczmarek <michalkaczmarek788@gmail.com>
2023-11-11 23:37:17 +0100

    Create constructor and refactor main class

--- src/main/java/pl/javastart/task/PlayStationFive.java
+++ src/main/java/pl/javastart/task/PlayStationFive.java
@@ -10,10 +10,24 @@ class PlayStationFive {
     int usbInputs;
     double sizeDisc;
     double price;
+
+    PlayStationFive(boolean blurayDrive, double height, double width, double length,
+                     String pad, int hdmiInputs, int usbInputs, double sizeDisc, double price) {
+        this.blurayDrive = blurayDrive;
+        this.height = height;
+        this.width = width;
+        this.length = length;
+        this.pad = pad;
+        this.hdmiInputs = hdmiInputs;
+        this.usbInputs = usbInputs;
+        this.sizeDisc = sizeDisc;
+        this.price = price;
+    }
+
     void printInfo() {
         System.out.println("Cena: " + price + " zł");
         System.out.println("Konsola posiada naped bluray: " + blurayDrive);
-        System.out.println("Wymiary konsoli: " + height + "x" + width + "x" + length);
+        System.out.println("Wymiary konsoli: " + height + "cmx" + width + "cmx" + length + "cm");
         System.out.println("Konsola jest w sprzedazy wraz z padem: " + pad);
         System.out.println("Konsola posiada " + hdmiInputs + " wejscia hdmi");
         System.out.println("Konsola posiada " + usbInputs + " wejscia usb");
